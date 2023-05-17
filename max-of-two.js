a97af8d79ebed1fc8ca87fa976cd6510cba38799
Alex Durrant <alexjdurrant@gmail.com>
2023-05-17 07:46:39 -0600

    Max of Two Lab

---
+++ max-of-two.js
@@ -0,0 +1,10 @@
+let x = 1
+let y = 2
+
+if (x > y) {
+    console.log(`x is greater than y at ${x}`)
+} else if (x < y){
+    console.log (`y is greater than x at ${y}`)
+} else if (x === y){
+    console.log (`The values are the same`)
+}
